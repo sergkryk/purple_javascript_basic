c65f781cbdf37f83563d492bb230dfc32890ffc6
sergkryk <sergkryk@ya.ru>
2023-05-31 10:06:09 +0300

    adds 11-date-array

---
+++ 11-date-array/main.js
@@ -0,0 +1,14 @@
+const parsed = ["10-02-2022", "test", "11/12/2023", "00/13/2022", "41/12/2023", '13.13.1990'];
+
+const dates = parsed.reduce((acc, stringToCheck) => {
+  if (Date.parse(stringToCheck)) {
+		const milliseconds = Date.parse(stringToCheck);
+		const date = new Date(milliseconds).toLocaleDateString('ru-RU');
+    acc.push(date);
+    return acc;
+  }
+  return acc;
+}, []);
+
+console.log(dates);
+
