42b8f9e9034b7dee83563e8c2ece2612ec8f8570
sergkryk <sergkryk@ya.ru>
2023-06-02 13:46:40 +0300

    adds date 30/02/2020

--- 11-date-array/main.js
+++ 11-date-array/main.js
@@ -1,4 +1,4 @@
-const parsed = ["10-02-2022", "test", "11/12/2023", "00/13/2022", "41/12/2023", '13.13.1990'];
+const parsed = ["10-02-2022", "test", "11/12/2023", "00/13/2022", "41/12/2023", '13.13.1990', '30/02/2020'];
 
 const dates = parsed.reduce((acc, stringToCheck) => {
   if (Date.parse(stringToCheck)) {
