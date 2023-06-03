a923c434a4c61c4cc4b5825bfa2bddece91a8e4f
sergkryk <sergkryk@ya.ru>
2023-06-03 16:20:54 +0300

    fixes after review

--- 9-sort-loops/main.js
+++ 9-sort-loops/main.js
@@ -1,14 +1,13 @@
-const arr = [1, 40, -5, 10, 0, 1, 30];
+const arr = [1, 40, -5, 10, 0, 1, 30, 500, -100];
 
 function sort(numbers) {
   for (let i = 0; i < numbers.length; i++) {
     if (i === numbers.length - 1) {
       break;
     }
-    const current = numbers[i];
-    if (current > numbers[i + 1]) {
-      numbers[i] = numbers[i + 1];
-      numbers[i + 1] = current;
+
+    if (numbers[i] > numbers[i + 1]) {
+      [numbers[i], numbers[i + 1]] = [numbers[i + 1], numbers[i]];
       sort(numbers);
     }
   }
