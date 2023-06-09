131501dfc3127073c7ed363d6446640bc23cda8c
sergkryk <sergkryk@ya.ru>
2023-06-09 15:14:19 +0300

    fixes after review

--- 8-crypto/main.js
+++ 8-crypto/main.js
@@ -3,28 +3,21 @@ function crypto(password) {
     return null;
   }
   const chars = password.split("");
-  const half = Math.floor(chars.length / 2)
-  const partOne = chars.slice(0, half)
-  const partTwo = chars.slice(half)
+  const half = Math.floor(chars.length / 2);
+  const partOne = chars.slice(0, half).reverse();
+  const partTwo = chars.slice(half);
+  const partTwoLength = partTwo.length;
 
-  // const reversed = partOne.reduceRight((acc, el) => {
-  //   acc.push(el);
-  //   return acc;
-  // }, []);
-  const reversed = partOne.reverse();
+  [partTwo[0], partTwo[partTwoLength - 1]] = [partTwo[partTwoLength - 1], partTwo[0]];
 
-  const replaced = partTwo.reduce((acc, el, elIndex, elements) => {
-    acc[elements.length - (1 + elIndex)] = el;
-    return acc;
-  }, [])
-
-  return [...replaced, ...reversed].join('');
+  return [...partOne, ...partTwo].join("");
 }
 
 function check(crypted, original) {
-  const decrypted = crypto(crypted);
-  return decrypted === original;
+  return crypto(crypted) === original;
 }
 
-console.log(check("tlashtiwdrowssapgnolyrev", "verylongpasswordwithsalt"));
-console.log(check("drowssap", "password"));
+console.log(check("ssapgnolyrevtordwithsalw", "verylongpasswordwithsalt"));
+console.log(check("ssapdorw", "password"));
+console.log(crypto("password"));
+console.log(crypto("verylongpasswordwithsalt"));
